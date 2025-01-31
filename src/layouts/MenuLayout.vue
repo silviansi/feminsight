33114016c5fddb09938937dd9e478a4904f41ae3
silviansi <silviansi24@gmail.com>
2025-01-31 21:13:59 +0700

    add layout for landing page and layout for calch

---
+++ src/layouts/MenuLayout.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Navbar from '../components/Navbar.vue';
+
+</script>
+
+<template>
+    <div>
+        <Navbar />
+        <main class="min-h-screen flex justify-center items-center">
+            <slot />
+        </main>
+    </div>
+</template>
