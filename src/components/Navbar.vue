489abffdc87845744a1c4513f709f35520fce9a4
silviansi <silviansi24@gmail.com>
2025-02-06 20:28:27 +0700

    change list navbar and add shadow

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,15 @@
 <template>
-    <nav class="fixed top-0 left-0 w-full px-6 md:px-10 py-4 flex justify-between items-center bg-white/30 backdrop-blur-md z-50">
+    <nav class="fixed top-0 left-0 w-full px-6 md:px-10 py-4 flex justify-between items-center bg-white/30 backdrop-blur-md z-50 shadow-md">
       
         <!-- Logo -->
         <div class="text-pink-600 font-bold text-2xl">FemInsight</div>
   
         <!-- Menu Desktop -->
         <ul class="hidden md:flex space-x-6">
-            <li><router-link to="/" class="text-gray-700 hover:text-pink-600">Home</router-link></li>
-            <li><router-link to="/bmi" class="text-gray-700 hover:text-pink-600">BMI</router-link></li>
+            <li><router-link to="/" class="text-gray-700 hover:text-red-600">Home</router-link></li>
             <li><router-link to="/menstruasi" class="text-gray-700 hover:text-pink-600">Menstruasi</router-link></li>
-            <li><router-link to="/hpl" class="text-gray-700 hover:text-pink-600">HPL</router-link></li>
+            <li><router-link to="/bmi" class="text-gray-700 hover:text-blue-500">BMI</router-link></li>
+            <li><router-link to="/hpl" class="text-gray-700 hover:text-green-500">HPL</router-link></li>
         </ul>
   
         <!-- Tombol Menu Mobile -->
@@ -19,10 +19,10 @@
   
         <!-- Menu Mobile -->
         <div v-if="isOpen" class="absolute top-16 left-0 w-full bg-white/90 backdrop-blur-md shadow-md py-4 flex flex-col items-center space-y-4 md:hidden">
-            <router-link to="/" class="text-gray-700 hover:text-pink-600" @click="isOpen = false">Home</router-link>
-            <router-link to="/bmi" class="text-gray-700 hover:text-pink-600" @click="isOpen = false">BMI</router-link>
+            <router-link to="/" class="text-gray-700 hover:text-red-600" @click="isOpen = false">Home</router-link>
             <router-link to="/menstruasi" class="text-gray-700 hover:text-pink-600" @click="isOpen = false">Menstruasi</router-link>
-            <router-link to="/hpl" class="text-gray-700 hover:text-pink-600" @click="isOpen = false">HPL</router-link>
+            <router-link to="/bmi" class="text-gray-700 hover:text-blue-500" @click="isOpen = false">BMI</router-link>
+            <router-link to="/hpl" class="text-gray-700 hover:text-green-500" @click="isOpen = false">HPL</router-link>
         </div>
     </nav>
 </template>
