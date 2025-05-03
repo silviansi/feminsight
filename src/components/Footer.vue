fdba59c734164fec60bd042ff3074bdc18c726f9
silviansi <silviansi24@gmail.com>
2025-05-03 20:37:20 +0700

    add footer section

---
+++ src/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+<footer class="relative z-10 w-full text-center py-8">
+    <p class="text-sm text-[#6a4c46] mb-2">© 2025 FemInsight. All rights reserved.</p>
+    <p class="text-sm text-[#6a4c46]">
+        Dibuat dengan <span class="text-pink-500">♥</span> untuk perempuan Indonesia.
+    </p>
+</footer>
+</template>
