1d4941a82e27ffac9e1040dee85cafe8507243d9
silviansi <silviansi24@gmail.com>
2025-05-04 14:57:04 +0700

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
-import LandingLayouts from '../layouts/LandingLayouts.vue';
 import MenstruasiView from '../views/Menstruasi.vue';
 import BMIView from '../views/BMI.vue';
 import HPLView from '../views/HPL.vue';
-import MenuLayout from '../layouts/MenuLayout.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -12,22 +10,18 @@ const router = createRouter({
     {
       path: '/',
       component: Home,
-      meta: { layout: LandingLayouts },
     },
     {
       path: '/bmi',
       component: BMIView,
-      meta: { layout: MenuLayout },
     },
     {
       path: '/menstruasi',
       component: MenstruasiView,
-      meta: { layout: MenuLayout },
     },
     {
       path: '/hpl',
       component: HPLView,
-      meta: { layout: MenuLayout },
     }
   ],
 })
